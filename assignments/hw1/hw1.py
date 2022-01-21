0bc025af1a748393e5c202b3be0845d9471c1346
Briana <addisonbh@g.cofc.edu>
2022-01-21 13:56:35 -0500

    Completed homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,51 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Briana Addison
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: I am solving multiple problems by using user input
 
-Certification of Authenticity:
-<include one of the following>
+
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+   length = eval(input("Enter the length: "))
+   width =  eval(input("Enter the width: "))
+   height =  eval(input("Enter the height: "))
+   volume = length * width * height
+   print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the player's total shots: "))
+    player_shots = eval(input("Enter how many shots the player made: "))
+    shooting_percentage = total_shots * player_shots
+    print("Shooting Percentage:", shooting_percentage, "%")
 
 
 def coffee():
-    pass
+    coffee_cost_pound = 10.50
+    coffee_shipping = 0.86
+    coffee_fixed_price = 1.50
+    num_of_pounds = eval(input("How many pounds of coffee would you like?"))
+    coffee_total = (coffee_cost_pound * num_of_pounds) + (coffee_shipping * num_of_pounds ) + (coffee_fixed_price)
+    print("Your total is: $", coffee_total)
+
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many kilometers did you travel?"))
+    miles = kilometers * 0.62111801
+
+    print("That's", round(miles, 2), "miles!")
 
 
 if __name__ == '__main__':
