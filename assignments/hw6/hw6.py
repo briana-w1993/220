81427df598e24099b7494abb8baee01de1a6cb81
Briana <addisonbh@g.cofc.edu>
2022-02-25 20:19:52 -0500

    Completed homework 6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,42 +1,69 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Briana Addison
+hw6.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: This program solves several problems using strings, ord, and chr
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
+
+import math
+
+
 def cash_converter():
-    pass
+    integer = eval(input("enter an integer: "))
+    integer = str(integer)
+    cash = "That is {}{}{}".format("$", integer, ".00")
+    print(cash)
 
 
 def encode():
-    pass
+    message = input("enter a message")
+    user_key = eval(input("enter a key"))
+    for i in range(len(message)):
+        translate_message = message[i]
+        translate_message = ord(translate_message)
+        shift = translate_message + user_key
+        final_message = chr(shift)
+        print(final_message, end="")
 
 
 def sphere_area(radius):
-    pass
+    area = (4 * math.pi * (radius ** 2))
+    return area
 
 
 def sphere_volume(radius):
-    pass
+    volume = (4/3 * math.pi * (radius ** 3))
+    return volume
 
 
 def sum_n(number):
-    pass
+    sum = 0
+    for i in range(1, number + 1):
+        sum += i
+    return sum
 
 
 def sum_n_cubes(number):
-    pass
+    sum = 0
+    for i in range(1, number + 1):
+        sum += i ** 3
+    return sum
 
 
 def encode_better():
-    pass
+    phrase = input("Enter phrase")
+    key = input("Enter key")
+    for i in range(len(phrase)):
+        character_val = ord(phrase[i]) - 65
+        key_val = ord(key[i % len(key)]) - 65
+        encode_val = ((character_val + key_val) % 57) + 64
+        encode_text = chr(encode_val)
+        print(encode_text, end="")
 
 
 if __name__ == '__main__':
